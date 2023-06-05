a9c9f65bb1f612285cfa2063f50e03c302686546
TheoRafuse <124588617+TheoRafuse@users.noreply.github.com>
2023-06-05 15:04:15 +0000

    halfway done CPT-06

---
+++ js/menuScene.js
@@ -0,0 +1,9 @@
+/* global Phaser */
+
+//Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+//Created by: Mr. Coxall
+//Edited by: Theo Rafuse
+//Created on: Sep 2020
+//Edited on: June 2023
+//This is the Menu Scene
--- js/titleScene.js
+++ js/titleScene.js
@@ -17,6 +17,14 @@ class TitleScene extends Phaser.Scene {
   */
   constructor() {
     super({ key: "titleScene" })
+
+    this.titleSceneBackgroundImage = null
+    this.titleSceneText = null
+    this.titleSceneTextStyle = {
+      font: "200px Times",
+      fill: "#fde4b9",
+      align: "center",
+    }
   }
 
   /** 
@@ -35,6 +43,7 @@ class TitleScene extends Phaser.Scene {
   */
   preload(){
     console.log("Title Scene")
+    this.load.image("titleSceneBackground", "assets/aliens_screen_image.jpg")
   }
 
   /** 
@@ -43,7 +52,15 @@ class TitleScene extends Phaser.Scene {
   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
   */
   create(data) {
-    //pass
+    this.titleSceneBackgroundImage = this.add
+      .sprite(0, 0, "titleSceneBackground")
+      .setScale(2.75)
+    this.titleSceneBackgroundImage.x = 1920 / 2
+    this.titleSceneBackgroundImage.y = 1080 / 2
+
+    this.titleSceneText = this.add
+    .text(1920 / 2, 1080 / 2 + 350, "Close Quasar Combat", this.titleSceneTextStyle)
+    .setOrigin(0.5)
   }
 
   /** 
